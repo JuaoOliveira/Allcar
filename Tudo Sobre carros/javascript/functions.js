7b2bec9f04a6b438871cd982ef9709317a9627a2
JuaoOliveira <jvoliveira3011@gmail.com>
2018-02-11 19:09:45 -0300

    Form do anúncio funcional

--- Tudo Sobre carros/javascript/functions.js
+++ Tudo Sobre carros/javascript/functions.js
@@ -17,19 +17,38 @@ function showAdd() {
         document.querySelector(".ad").style.display = "none";
         clearInterval(interval);
     })
-}   
+}
 
 //Validar formulário do anuncio
-//document.querySelector("#input_name")
+$("#input_name").keyup(function() {
+    var emailAd = form_ad.email_ad.value.trim();
+    if (emailAd == "" || emailAd.indexOf('@') == -1 || emailAd.substr(emailAd.length - 4)!== ".com") {
+        $(".invalidData").text("Digite o e-mail corretamente!");
+    } else {
+        $(".invalidData").text("");
+    }
+})
+
+//Validar anuncio
+document.querySelector(".input_btn").addEventListener("click", function(evt) {
+    var emailAd = form_ad.email_ad.value.trim();
+    if (emailAd.length == 0 || emailAd.indexOf('@') == -1 || 
+    emailAd.substr(emailAd.length - 4) !== ".com") {
+        alert("Digite o e-mail corretamente!");
+        evt.preventDefault();
+    } 
+})
 
 //Colocar data atual no campo de data
 $(document).ready(function() {
     var nascMask = $("#nasc");
-    nascMask.mask('00/00/0000', {placeholder: 'DD/MM/AAAA'});
+    nascMask.mask('00/00/0000', {
+        placeholder: 'DD/MM/AAAA'
+    });
 })
 
 //Validando o formulário do fale conosco
-document.querySelector("#btn_enviar").addEventListener("click", function(e) {
+document.querySelector("#btn_enviar").addEventListener("click", function (e) {
     var name = form1.tNome.value.trim();
     var email = form1.tEmail.value.trim();
     var nasc = form1.tNasc.value.trim();
@@ -63,7 +82,7 @@ document.querySelector("#btn_enviar").addEventListener("click", function(e) {
 })
 
 //Validando o nome antes de apertar o botão
-$("#nome").keyup(function(e) {
+$("#nome").keyup(function (e) {
     var name = form1.tNome.value.trim();
     if (!regex.test(name)) {
         $(".invalidName").text("Preencha o campo corretamente!");
@@ -74,7 +93,7 @@ $("#nome").keyup(function(e) {
 })
 
 //Validando o campo E-mail antes do botão
-$("#email").keyup(function(e) {
+$("#email").keyup(function (e) {
     var email = form1.tEmail.value.trim();
     if (email == "" || email.indexOf("@") == -1 || email.indexOf(".com") == -1 || email.substr(email.length - 4) !== ".com") {
         $(".invalidEmail").text("Preencha o campo corretamente!");
@@ -85,7 +104,7 @@ $("#email").keyup(function(e) {
 })
 
 //Validar o campo mensagem automaticamente
-$("#msg").keyup(function(e) {
+$("#msg").keyup(function (e) {
     var msg = form1.cMsg.value.trim();
     var showWordCounter = form1.cMsg.value.length;
     $(".word-counter").text("caracteres digitados: " + showWordCounter);

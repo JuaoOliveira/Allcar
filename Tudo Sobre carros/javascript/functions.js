4219bbd40fb67cc44aaa7429c0ea86717a0fecda
JuaoOliveira <jvoliveira3011@gmail.com>
2018-02-11 21:27:05 -0300

    Identação do arquivo functions.js

--- Tudo Sobre carros/javascript/functions.js
+++ Tudo Sobre carros/javascript/functions.js
@@ -25,7 +25,7 @@ function showAdd() {
 //Validar formulário do anuncio
 $("#input_name").keyup(function() {
     var emailAd = form_ad.email_ad.value.trim();
-    if (emailAd == "" || emailAd.indexOf('@') == -1 || emailAd.substr(emailAd.length - 4)!== ".com") {
+    if (emailAd == "" || emailAd.indexOf('@') == -1 || emailAd.substr(emailAd.length - 4) !== ".com") {
         $(".invalidData").text("Digite o e-mail corretamente!");
     } else {
         $(".invalidData").text("");
@@ -37,8 +37,7 @@ var btnInput = document.querySelector(".ad");
 if (btnInput != null) {
     document.querySelector(".input_btn").addEventListener("click", function(evt) {
         var emailAd = form_ad.email_ad.value.trim();
-        if (emailAd.length == 0 || emailAd.indexOf('@') == -1 ||
-        emailAd.substr(emailAd.length - 4) !== ".com") {
+        if (emailAd.length == 0 || emailAd.indexOf('@') == -1 || emailAd.substr(emailAd.length - 4) !== ".com") {
             alert("Digite o e-mail corretamente!");
             evt.preventDefault();
         }
@@ -49,49 +48,44 @@ if (btnInput != null) {
 $(document).ready(function() {
     var form = document.querySelector("#fContato");
     var nascMask = $("#nasc");
-    nascMask.mask('00/00/0000', {
-        placeholder: 'DD/MM/AAAA'
-    });
+    nascMask.mask('00/00/0000', {placeholder: 'DD/MM/AAAA'});
 
-
-//Validando o formulário do fale conosco
-if(form != null){
-document.querySelector("#btn_enviar").addEventListener("click", function (e) {
-    var name = form1.tNome.value.trim();
-    var email = form1.tEmail.value.trim();
-    var nasc = form1.tNasc.value.trim();
-    var msg = form1.cMsg.value.trim();
-
-    console.log(name)
-    if (name === "" || !regex.test(name)) {
-        alert("Preencha o(s) campo \"nome\" corretamente!" + name);
-        $("#nome").css("border", "1px solid red");
-        document.form1.tNome.focus();
-        e.preventDefault();
+    //Validando o formulário do fale conosco
+    if (form != null) {
+        document.querySelector("#btn_enviar").addEventListener("click", function(e) {
+            var name = form1.tNome.value.trim();
+            var email = form1.tEmail.value.trim();
+            var nasc = form1.tNasc.value.trim();
+            var msg = form1.cMsg.value.trim();
+            if (name === "" || !regex.test(name)) {
+                alert("Preencha o(s) campo \"nome\" corretamente!" + name);
+                $("#nome").css("border", "1px solid red");
+                document.form1.tNome.focus();
+                e.preventDefault();
+            }
+            if (email === "" || email.indexOf('@') === -1 || email.indexOf(".com") === -1 || email.substr(email.length - 4) !== ".com") {
+                alert("Preencha o(s) campo \"email\" corretamente!");
+                $("#email").css("border", "1px solid red");
+                document.form1.tEmail.focus();
+                e.preventDefault();
+            }
+            if (nasc === "") {
+                alert("Preencha o(s) campo \"Data de nascimento\" corretamente!");
+                $("#nasc").css("border", "1px solid red");
+                document.form1.tNasc.focus();
+                e.preventDefault();
+            }
+            if (msg.length === 0) {
+                alert("Preencha o(s) campo \"Mensagem\" corretamente!");
+                $("#msg").css("border", "1px solid red");
+                document.form1.cMsg.focus();
+                e.preventDefault();
+            }
+        })
     }
-    if (email === "" || email.indexOf('@') === -1 || email.indexOf(".com") === -1 || email.substr(email.length - 4) !== ".com") {
-        alert("Preencha o(s) campo \"email\" corretamente!");
-        $("#email").css("border", "1px solid red");
-        document.form1.tEmail.focus();
-        e.preventDefault();
-    }
-    if (nasc === "") {
-        alert("Preencha o(s) campo \"Data de nascimento\" corretamente!");
-        $("#nasc").css("border", "1px solid red");
-        document.form1.tNasc.focus();
-        e.preventDefault();
-    }
-    if (msg.length === 0) {
-        alert("Preencha o(s) campo \"Mensagem\" corretamente!");
-        $("#msg").css("border", "1px solid red");
-        document.form1.cMsg.focus();
-        e.preventDefault();
-    }
-})
-}
 })
 //Validando o nome antes de apertar o botão
-$("#nome").keyup(function (e) {
+$("#nome").keyup(function(e) {
     var name = form1.tNome.value.trim();
     if (!regex.test(name)) {
         $(".invalidName").text("Preencha o campo corretamente!");
@@ -102,7 +96,7 @@ $("#nome").keyup(function (e) {
 })
 
 //Validando o campo E-mail antes do botão
-$("#email").keyup(function (e) {
+$("#email").keyup(function(e) {
     var email = form1.tEmail.value.trim();
     if (email == "" || email.indexOf("@") == -1 || email.indexOf(".com") == -1 || email.substr(email.length - 4) !== ".com") {
         $(".invalidEmail").text("Preencha o campo corretamente!");
@@ -113,7 +107,7 @@ $("#email").keyup(function (e) {
 })
 
 //Validar o campo mensagem automaticamente
-$("#msg").keyup(function (e) {
+$("#msg").keyup(function(e) {
     var msg = form1.cMsg.value.trim();
     var showWordCounter = form1.cMsg.value.length;
     $(".word-counter").text("caracteres digitados: " + showWordCounter);
